607c25febb4d21a5a135c1e027f41e8f504c6c66
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-09-30 01:45:44 +0530

    feat(user.common.emacs): add autocompletion

--- src/user/common/emacs/init.el
+++ src/user/common/emacs/init.el
@@ -22,3 +22,8 @@
 (vertico-mode)
 (marginalia-mode)
 (evil-global-set-key 'normal (kbd "<leader>spc") 'consult-fd)
+
+;;; Autocompletion
+(electric-pair-mode) ; Autocomplete brackets, quotes, etc.
+(global-corfu-mode)
+(setq corfu-auto t)
