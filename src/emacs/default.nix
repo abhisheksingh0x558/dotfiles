ad5499c167e3e2a614c3d3ded9f7555f441a9cff
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-09-30 01:46:26 +0530

    feat(emacs): add git integration

--- src/emacs/default.nix
+++ src/emacs/default.nix
@@ -37,6 +37,9 @@
         # Autocompletion
         corfu # TODO: document this here and in init.el
         cape # TODO: document this here and in init.el
+
+        # VCS integration
+        magit # Git client
       ];
   };
 
