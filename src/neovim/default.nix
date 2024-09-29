f418093946c054894c646f863102241d0970301c
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-09-30 01:27:56 +0530

    feat(neovim): adjust code style

--- src/neovim/default.nix
+++ src/neovim/default.nix
@@ -1,7 +1,24 @@
-{
+{ pkgs, ... }:
+let
+  # Init file
+  initFile = "init.lua";
+  initPath = pkgs.stdenvNoCC.mkDerivation rec {
+    name = "neovim";
+    src = ./init.fnl;
+    dontUnpack = true;
+    installPhase = ''
+      mkdir -p $out
+      fennel --compile ${src} > $out/${initFile}
+    '';
+    nativeBuildInputs = [ pkgs.luajitPackages.fennel ];
+  };
+in {
   # Editor
   programs.neovim = {
     enable = true;
     defaultEditor = true; # Default editor
   };
+
+  xdg.configFile."nvim/init.lua".source = builtins.toString initPath
+    + "/${initFile}"; # Init file
 }
---
+++ src/neovim/init.fnl
@@ -0,0 +1,8 @@
+(local vim _G.vim)
+(local g vim.g)
+
+;;; Code style
+;; Editorconfig integration
+(set g.editorconfig true)
+
+{}
