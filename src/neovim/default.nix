e582b8caa8e2dd5445536f1083a722269f696efa
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-09-30 01:27:56 +0530

    feat(neovim): adjust code style

--- src/neovim/default.nix
+++ src/neovim/default.nix
@@ -1,7 +1,25 @@
-{
+{ pkgs, ... }:
+let
+  # Init file
+  initFile = "init.lua";
+  initPath = pkgs.stdenvNoCC.mkDerivation rec {
+    name = "neovim";
+    src = ./init.fnl;
+    dontUnpack = true;
+    installPhase = ''
+      mkdir -p $out
+      fennel --compile ${src} > $out/${initFile}
+    '';
+    nativeBuildInputs = [ pkgs.luajitPackages.fennel ];
+  };
+in {
   # Editor
   programs.neovim = {
     enable = true;
     defaultEditor = true; # Default editor
   };
+
+  # Init file
+  xdg.configFile."nvim/${initFile}".source = builtins.toString initPath
+    + "/${initFile}";
 }
