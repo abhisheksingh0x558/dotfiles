6e7b0e593a7ac940a02bda67ad0b570ee96971a3
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-09-29 17:12:27 +0530

    feat(emacs): adjust code style

--- src/emacs/default.nix
+++ src/emacs/default.nix
@@ -1,4 +1,15 @@
-{
+{ lib, ... }: {
   # Editor
-  programs.emacs.enable = true;
+  programs.emacs = {
+    enable = true;
+
+    extraPackages = epkgs:
+      with epkgs;
+      [
+        # Code style
+        (lib.warn "editorconfig is merged to upstream in emacs 30" editorconfig)
+      ];
+  };
+
+  xdg.configFile."emacs/init.el".source = ./init.el; # Init file
 }
