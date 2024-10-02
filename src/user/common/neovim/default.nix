f1c81ed93a442e1fe88465b8d390ef89735a210d
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-02 17:53:24 +0530

    feat(user.common.neovim): add keymaps

--- src/user/common/neovim/default.nix
+++ src/user/common/neovim/default.nix
@@ -24,6 +24,26 @@ in {
   programs.neovim = {
     enable = true;
     defaultEditor = true; # Default editor
+
+    plugins = with pkgs.vimPlugins; [
+      # Keymaps
+      unimpaired-nvim # Pairs of bracket keymaps
+      comment-nvim # Manipulate comments
+      nvim-surround # Manipulate surrounding pairs
+      substitute-nvim # Exchange operator
+      text-case-nvim # Manipulate text cases
+      align-nvim # Manipulate alignments
+      dial-nvim # Increment and decrement operator
+      treesj # Split and join operator
+      nvim-gomove # Move and duplicate block operator
+      flash-nvim # Navigate with search labels
+      grug-far-nvim # Find and replace
+      nvim-various-textobjs # Text objects
+      neogen # Annotation generator
+      other-nvim # Alternative files
+      scretch-nvim # Scratch files
+      hydra-nvim # Sticky keymaps
+    ];
   };
 
   # Initialization files
@@ -31,5 +51,7 @@ in {
     "init" # Initialization file
     "lua/lib" # Library
     "lua/option" # Options
+    "lua/keymap" # Keymaps
+    "lua/plugin/plugin" # Plugins
   ]);
 }
