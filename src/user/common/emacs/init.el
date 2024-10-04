b66dff6e907fd50029879a152e5ce178e55a56d6
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-04 23:04:40 +0530

    feat(user.common.emacs): add keymaps

--- src/user/common/emacs/init.el
+++ src/user/common/emacs/init.el
@@ -2,3 +2,12 @@
 
 ;;; Code style
 (editorconfig-mode) ; Editorconfig integration
+
+;;; Keymaps
+;; Vi layer
+(setq evil-want-keybinding nil)
+(evil-mode)
+(evil-collection-init)
+(general-create-definer leader-definer :prefix "SPC")
+(general-create-definer local-leader-definer :prefix "\\")
+
