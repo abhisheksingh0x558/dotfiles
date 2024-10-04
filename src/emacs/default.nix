61f2ccddf1e2b391a05f4d56acfb32107407b58d
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-04 23:04:40 +0530

    feat(emacs): add key mappings

--- src/emacs/default.nix
+++ src/emacs/default.nix
@@ -1,6 +1,24 @@
 {
   # Editor
-  programs.emacs.enable = true;
+  programs.emacs = {
+    enable = true;
+
+    extraPackages = epkgs:
+      with epkgs; [
+        # Key mappings
+        # Vi layer
+        evil
+        evil-collection
+        evil-commentary # Manipulate comments
+        evil-surround # Manipulate surrounding pairs
+        evil-exchange # Exchange operator
+        evil-lion # Manipulate alignments
+        # TODO: Check if this is required
+        evil-snipe # Navigate with search labels
+        # TODO: Replace with embark/transient/hercules
+        hydra # Sticky key mappings
+      ];
+  };
 
   # Init file
   xdg.configFile."emacs/init.el".source = ./init.el;
--- src/emacs/init.el
+++ src/emacs/init.el
@@ -2,3 +2,11 @@
 
 ;;; Code style
 (editorconfig-mode) ; Editorconfig integration
+
+;;; Key mappings
+;; Vi layer
+(setq evil-want-keybinding nil)
+(evil-mode)
+(evil-collection-init)
+(evil-set-leader 'normal (kbd " "))
+(evil-set-leader 'normal (kbd "\\"))
