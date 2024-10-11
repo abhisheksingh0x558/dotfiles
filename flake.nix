e6494ca170d39fdfd5ab76c9b76ad360839052e0
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-11 17:34:50 +0530

    feat: setup nixos

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  outputs = { nixpkgs, nixos-hardware, ... }:
+    let
+      cfg = import ./cfg/config.nix;
+      system = "x86_64-linux";
+      firmware = { pkgs, ... }: {
+        # NOTE: Firmware for macbook to enable wifi
+        hardware.firmware = [
+          (pkgs.stdenvNoCC.mkDerivation rec {
+            name = "brcm-firmware";
+            src = ./res/firmware.tar;
+            unpackPhase = ''
+              mkdir -p $out/lib/firmware/brcm
+              tar -xf ${src} -C $out/lib/firmware/brcm
+            '';
+          })
+        ];
+      };
+    in {
+      # NixOS
+      nixosConfigurations.${cfg.hostname} = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./src/system.nix
+          ./gen/hardware.nix # FIXME: Generate with `nixos-generate-config`
+          nixos-hardware.nixosModules.apple-t2 # NOTE: Kernal for macbook to enable wifi, keyboard, touchpad, etc.
+          firmware
+        ];
+      };
+    };
+}
