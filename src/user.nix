289af87e53785053ec566574db65af32b21d5ea7
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-11 18:09:54 +0530

    feat: install fennel tools

--- src/user.nix
+++ src/user.nix
@@ -45,6 +45,12 @@ in {
 
     # GUIs
     bitwarden-desktop # Password manager
+
+    # Fennel
+    # TODO: Install linter
+    luajitPackages.fennel
+    fennel-ls # Language server
+    fnlfmt # Formatter
   ];
 
   # Fonts
