eaee284b1f63d0c2fedad4219e82c024ee4cce5b
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-11 18:13:16 +0530

    feat(user.common.emacs): install emacs

--- src/user/common/overlay.nix
+++ src/user/common/overlay.nix
@@ -1,8 +1,13 @@
 { inputs, pkgs, ... }: {
   nixpkgs.overlays = [
+    inputs.emacs-overlay.overlays.default # Emacs nightly overlay
+
     (_: _: {
       # Wezterm nightly overlay
       wezterm = inputs.wezterm.packages.${pkgs.system}.default;
+
+      # Emacs nightly overlay
+      emacs = inputs.emacs-overlay.packages.${pkgs.system}.emacs-unstable;
     })
   ];
 }
