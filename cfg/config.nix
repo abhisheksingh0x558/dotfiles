7c6e9aba28a98f0d80fe1883fdeaa21049ba0b8f
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-11 18:01:53 +0530

    feat(user.common.git): install git

--- cfg/config.nix
+++ cfg/config.nix
@@ -2,6 +2,15 @@
   # Host
   hostname = "macbookpro";
 
-  # Default user
-  user.default.username = "abhisheksingh";
+  user = {
+    # Default user
+    default = {
+      username = "abhisheksingh";
+      name = "Abhishek Singh";
+      email = "abhisheksingh0x558@proton.me";
+    };
+
+    # Work user
+    nightfallai.email = "abhishek@nightfall.ai";
+  };
 }
