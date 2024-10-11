9b5b43c310b0389280e28175fc2e79fa4a86e616
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-11 18:01:08 +0530

    feat: adjust code style

--- src/user.nix
+++ src/user.nix
@@ -38,4 +38,23 @@ in {
 
   # TUIs
   programs.zoxide.enable = true; # Change directory
+
+  # Code style
+  editorconfig = {
+    enable = true;
+
+    settings = {
+      # All files
+      "*" = {
+        charset = "utf-8"; # File character encoding
+        end_of_line = "lf"; # Line ending file format
+        indent_size = "tab"; # Mimic tab size
+        indent_style = "tab"; # Indent with tabs
+        insert_final_newline = true; # Insert newline at end of file
+        max_line_length = 120; # Maximum length of line
+        tab_width = 2; # Tab size
+        trim_trailing_whitespace = true; # Remove whitespaces at end of line
+      };
+    };
+  };
 }
