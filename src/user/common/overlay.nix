d35cf4c761613c190b13e56245e309ac91a5f487
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-11 18:14:17 +0530

    feat(user.common.neovim): install neovim

--- src/user/common/overlay.nix
+++ src/user/common/overlay.nix
@@ -2,12 +2,18 @@
   nixpkgs.overlays = [
     inputs.emacs-overlay.overlays.default # Emacs nightly overlay
 
+    inputs.neovim-nightly-overlay.overlays.default # Neovim nightly overlay
+
     (_: _: {
       # Wezterm nightly overlay
       wezterm = inputs.wezterm.packages.${pkgs.system}.default;
 
       # Emacs nightly overlay
       emacs = inputs.emacs-overlay.packages.${pkgs.system}.emacs-unstable;
+
+      # Neovim nightly overlay
+      neovim-unwrapped =
+        inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
     })
   ];
 }
