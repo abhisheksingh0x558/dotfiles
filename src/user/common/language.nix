675b95bac878eff25e91f3a8f8e48dd7988f0401
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-11 18:10:50 +0530

    feat(user.common.language): install go tools

--- src/user/common/language.nix
+++ src/user/common/language.nix
@@ -23,5 +23,23 @@
     # TODO: Install formatter
     # TODO: Install linter
     zig
+
+    # Go
+    gopls # Language server
+    gofumpt # Formatter
+    go-tools # Linter
   ];
+
+  # Go
+  programs.go = {
+    enable = true;
+    # TODO: Use xdg.dataHome
+    goPath = ".local/share/go"; # Path for install global modules
+    # TODO: Use xdg.dataHome
+    goBin = ".local/share/bin"; # Path for installing global binaries
+    # Private modules
+    goPrivate = [
+      "github.com/watchtowerai" # Work repositories
+    ];
+  };
 }
