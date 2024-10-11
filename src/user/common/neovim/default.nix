5f2f5fc667c2ecf86f6a40522a4b3bd880d4d00e
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-11 18:14:17 +0530

    feat(user.common.neovim): install neovim

---
+++ src/user/common/neovim/default.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+let
+  # Transpile fennel to lua
+  transpile = files:
+    builtins.foldl' (files: file:
+      let
+        fileName = builtins.baseNameOf file;
+        filePath = pkgs.stdenvNoCC.mkDerivation rec {
+          name = "neovim";
+          src = ./${fileName} + ".fnl";
+          dontUnpack = true;
+          installPhase = ''
+            mkdir -p $out
+            fennel --compile ${src} > $out/${fileName}.lua
+          '';
+          nativeBuildInputs = [ pkgs.luajitPackages.fennel ];
+        };
+      in files // {
+        "nvim/${file}.lua".source = builtins.toString filePath
+          + "/${fileName}.lua";
+      }) { } files;
+in {
+  # Editor
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true; # Default editor
+  };
+
+  # Initialization files
+  xdg.configFile = (transpile [
+    "init" # Initialization file
+  ]);
+}
