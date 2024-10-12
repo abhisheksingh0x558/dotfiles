32186021c9885abff5c53a050978b08a67ef08f8
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-12 10:12:20 +0000

    nixos: configure keymaps

--- nixos.nix
+++ nixos.nix
@@ -1,4 +1,14 @@
-{
+let
+  # Keymaps
+  keydSettings = {
+    capslock =
+      "overload(control, escape)"; # Map to control if pressed with other keys and capslock to espace if pressed alone
+    enter =
+      "overload(control, enter)"; # Map enter to control if pressed with other keys
+    space =
+      "overload(alt, space)"; # Map space to alt if pressed with other keys
+  };
+in {
   # NixOS version
   # WARNING: Do not change this without referring to release notes for NixOS
   system.stateVersion = "24.11";
@@ -31,5 +41,38 @@
       "wheel" # Enable sudo to execute commands as root
       "networkmanager" # Allow configuring network
     ];
+  };
+
+  # Keymaps
+  # TODO: Add oneshot mappings for all modifiers and detect mouse/touchpad
+  # TODO: Reconsider space to alt mapping
+  services.keyd = {
+    enable = true;
+
+    keyboards = {
+      default = {
+        # Internal apple keyboard
+        ids = [ "*" ];
+        settings = {
+          main = {
+            escape = "capslock"; # Map escape to capslock
+          } // keydSettings;
+        };
+      };
+
+      external = {
+        # External reddragon keyboard
+        ids = [ "062a:8556" ];
+        settings = {
+          main = {
+            escape = "`"; # Map escape to backtick
+            "shift.escape" = "~"; # Map shift plus escape to tilda
+            meta = "layer(alt)"; # Map meta to alt
+            leftalt = "layer(meta)"; # Map leftalt to meta
+            rightalt = "layer(meta)"; # Map rightalt to meta
+          } // keydSettings;
+        };
+      };
+    };
   };
 }
