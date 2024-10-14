805f818b1466779c874800946a149b17e4eb7437
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-14 19:53:45 +0530

    feat: install proto tools

--- src/user.nix
+++ src/user.nix
@@ -81,6 +81,12 @@ in {
     kotlin-language-server # Language server
     ktfmt # Formatter
     ktlint # Linter
+
+    # Proto
+    # TODO: Install language server
+    # TODO: Install linter
+    protobuf
+    buf # Formatter
   ];
 
   # Fonts
