3dcc8c9ef7a53d3af1a6475f07b7b034f0549885
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-14 08:37:38 +0530

    feat(user.common.nushell): install nushell

--- src/system/macos/default.nix
+++ src/system/macos/default.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: rec {
   imports = [ ../common ];
 
   # Nix Darwin version
@@ -11,6 +11,13 @@
   # Homebrew
   homebrew.enable = true;
   homebrew.onActivation.cleanup = "zap"; # Uninstall stale applications
+
+  # Shells
+  # TODO: Merge this setting to upstream nixpkgs and remove from here
+  environment.shells = with pkgs; [ nushell ];
+
+  # TODO: Merge this setting to upstream nixpkgs and remove from here
+  environment.systemPackages = environment.shells;
 
   # Homebrew casks
   # TODO: Merge this setting to upstream nixpkgs and remove from here
