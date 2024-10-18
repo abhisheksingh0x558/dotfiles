fe57353c9184caa260e58bdfed79748f5d86be84
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-18 17:22:59 +0530

    feat(user.common.alacritty): improve ui and ux

--- src/user/common/alacritty.nix
+++ src/user/common/alacritty.nix
@@ -1,1 +1,11 @@
-{ }
+{
+  # Terminal
+  programs.alacritty = {
+    # UI/UX
+    settings = {
+      # Font
+      font.normal.family = "FiraCode Nerd Font";
+      font.size = 12.0;
+    };
+  };
+}
