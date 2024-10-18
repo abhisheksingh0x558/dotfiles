6f25264eb866f28346967619f417562514d79a6e
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-18 17:22:59 +0530

    feat: improve alacritty ui and ux

--- src/alacritty.nix
+++ src/alacritty.nix
@@ -1,4 +1,23 @@
-{
+{ pkgs, ... }:
+let
+  # Theme
+  # TODO: Add option for this in upstream Home Manager
+  mocha = pkgs.fetchurl {
+    url =
+      "https://raw.githubusercontent.com/catppuccin/alacritty/refs/heads/main/catppuccin-mocha.toml";
+    sha256 = "sha256-/N3rwIZ0SJBiE7TiBs4pEjhzM1f2hr26WXM3ifUzzOY=";
+  };
+in {
   # Terminal
-  programs.alacritty.enable = true;
+  programs.alacritty = {
+    enable = true;
+
+    # UI/UX
+    settings = {
+      import = [
+        mocha # Theme
+      ];
+      font.normal.family = "FiraCode Nerd Font"; # Font
+    };
+  };
 }
