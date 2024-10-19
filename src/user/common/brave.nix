122d75d8c3812161edc839f93ae0990c619810ee
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-19 23:41:09 +0530

    feat(user.common.brave): install extensions

--- src/user/common/brave.nix
+++ src/user/common/brave.nix
@@ -3,6 +3,14 @@ let
     "brave-browser.desktop" # Default browser
   ];
 in {
+  # Browser
+  programs.chromium = {
+    extensions = [
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # Ad blocker
+      { id = "nngceckbapebfimnlniiiahkandclblb"; } # Password manager
+    ];
+  };
+
   # TODO: Check if this is required in common module (for macos)
   xdg.mimeApps.defaultApplications = {
     # Default browser
