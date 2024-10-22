79658a8ac065593c2f453d6de558b83bcf925010
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-22 15:14:47 +0530

    feat(wezterm): improve ui and ux

--- src/wezterm/default.nix
+++ src/wezterm/default.nix
@@ -1,4 +1,22 @@
-{
+{ pkgs, ... }:
+let
+  # Init file
+  initFile = "wezterm.lua";
+  initPath = pkgs.stdenvNoCC.mkDerivation rec {
+    name = "wezterm";
+    src = ./wezterm.fnl;
+    dontUnpack = true;
+    installPhase = ''
+      mkdir -p $out
+      fennel --compile ${src} > $out/${initFile}
+    '';
+    nativeBuildInputs = [ pkgs.luajitPackages.fennel ];
+  };
+in {
   # Terminal
   programs.wezterm.enable = true;
+
+  # Init file
+  xdg.configFile."wezterm/${initFile}".source = builtins.toString initPath
+    + "/${initFile}";
 }
---
+++ src/wezterm/wezterm.fnl
@@ -0,0 +1,9 @@
+(local wezterm (require :wezterm))
+
+{;; Theme
+ :color_scheme "Catppuccin Mocha"
+ ;; Font
+ :font (wezterm.font "FiraCode Nerd Font")
+ :font_size 12
+ ;; Tabbar
+ :hide_tab_bar_if_only_one_tab true}
