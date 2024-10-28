3c4f236797cf5436c6bc4c1f25768272076c4c69
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-10-28 21:11:35 +0530

    feat(user.common.nushell): add git aliases

---
+++ src/user/common/common.nix
@@ -0,0 +1,13 @@
+{ lib, config, ... }:
+let gitAliasPrefix = "g";
+in {
+  # Shell aliases
+  options.shell.aliases = lib.mkOption {
+    type = lib.types.attrsOf lib.types.str;
+    default = {
+      "lg" = "lazygit";
+    } // lib.attrsets.mapAttrs' (name: value:
+      lib.attrsets.nameValuePair (gitAliasPrefix + name) ("git " + value))
+      config.programs.git.aliases;
+  };
+}
