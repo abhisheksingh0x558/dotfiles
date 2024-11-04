3805ddba2655b69152e26b9e8ad02cc245fc4a5f
Abhishek Singh <abhisheksingh0x558@proton.me>
2024-11-04 16:08:30 +0530

    feat(system.macos): add keymaps

--- src/system/macos/default.nix
+++ src/system/macos/default.nix
@@ -14,4 +14,10 @@
   # Homebrew
   homebrew.enable = true;
   homebrew.onActivation.cleanup = "zap"; # Uninstall stale applications
+
+  # Homebrew casks
+  # TODO: Merge this setting to upstream nixpkgs and remove from here
+  homebrew.casks = [
+    "karabiner-elements" # Keymaps
+  ];
 }
