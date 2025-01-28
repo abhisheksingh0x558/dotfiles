9ffb28e68921904798ff8bcc20a311562acc2d80
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-29 05:24:13 +0530

    nvim: adjust code style

--- chezmoi/dot_config/nvim/init.lua
+++ chezmoi/dot_config/nvim/init.lua
@@ -1,5 +1,9 @@
 local opt = vim.opt
 local fn = vim.fn
+local g = vim.g
 
 -- Plugin manager
 opt.rtp:prepend(fn.stdpath("data") .. "/lazy/lazy.nvim") -- Add plugin manager to runtime path
+
+-- Code style
+g.editorconfig = true -- Editorconfig integration
