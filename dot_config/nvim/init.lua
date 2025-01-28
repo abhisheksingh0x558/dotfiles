8922cfb44ce0859b12e8931771ece77bd6ef0e8a
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-29 05:24:13 +0530

    nvim: adjust code style

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,9 +1,13 @@
 local fs = vim.fs
 local opt = vim.opt
+local g = vim.g
 
 -- Plugin manager
 local lazypath = fs.normalize("~/.local/share/nvim/lazy/lazy.nvim")
 opt.rtp:prepend(lazypath) -- Load plugin manager
+
+-- Code style
+g.editorconfig = true -- EditorConfig integration
 
 require("lazy").setup({
 	-- Collection of plugins
