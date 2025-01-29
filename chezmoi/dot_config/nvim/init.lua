50b95e2041583f6e92471ef7cc2c8d032f3c83c7
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-29 16:01:24 +0530

    nvim: improve ui and ux

--- chezmoi/dot_config/nvim/init.lua
+++ chezmoi/dot_config/nvim/init.lua
@@ -3,6 +3,8 @@ local uv = vim.uv
 local system = vim.system
 local opt = vim.opt
 local g = vim.g
+local o = vim.o
+local cmd = vim.cmd
 
 -- Plugin manager
 local lazypath = fs.normalize("~/.local/share/nvim/lazy/lazy.nvim")
@@ -23,3 +25,48 @@ g.editorconfig = true -- Editorconfig integration
 -- Keymaps
 g.mapleader = " " -- Leader key
 g.maplocalleader = "\\" -- Local leader key
+
+-- UI/UX
+opt.shortmess:append("I") -- Disable intro message
+o.number = true -- Enable line numbering
+o.cursorline = true -- Enable line highlighting
+o.clipboard = "unnamedplus" -- Use system clipboard
+o.ignorecase = true -- Ignore case while searching
+o.smartcase = true -- Respect case if pattern has upper case characters
+o.splitright = true -- Put new window towards right on vertical split
+o.splitbelow = true -- Put new window towards bottom on horizontal split
+
+require("lazy").setup({
+	-- UI/UX
+	-- Theme
+	{
+		"catppuccin/nvim",
+		config = function()
+			cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
+	-- UI for diagnostics, references, telescope results, quickfix list and location list
+	{
+		"folke/trouble.nvim",
+		lazy = false,
+		opts = {
+			focus = true, -- Move cursor to trouble window when open
+		},
+		keys = {
+			-- Goto previous trouble entry
+			{
+				"[q",
+				function()
+					require("trouble").prev({ jump = true })
+				end,
+			},
+			-- Goto next trouble entry
+			{
+				"]q",
+				function()
+					require("trouble").next({ jump = true })
+				end,
+			},
+		},
+	},
+})
