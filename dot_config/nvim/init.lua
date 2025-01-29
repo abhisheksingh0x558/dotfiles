e3e497549a75c4efad6613052ff59ce5b6b7cbb1
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-29 16:01:24 +0530

    nvim: improve ui and ux

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,6 +2,8 @@ local fs = vim.fs
 local opt = vim.opt
 local g = vim.g
 local keymap = vim.keymap
+local o = vim.o
+local cmd = vim.cmd
 
 -- Plugin manager
 local lazypath = fs.normalize("~/.local/share/nvim/lazy/lazy.nvim")
@@ -18,6 +20,16 @@ keymap.set("n", "<c-j>", "<down>") -- Command line prefix history search downwar
 keymap.set("t", "<esc>", "<c-\\><c-n>") -- Switch from terminal mode to normal mode
 keymap.set("t", "<c-\\><c-n>", "<esc>") -- Send escape key to program running in terminal buffer
 
+-- UI/UX
+opt.shortmess:append("I") -- Disable intro message
+o.number = true -- Enable line numbering
+o.cursorline = true -- Enable line highlighting
+o.clipboard = "unnamedplus" -- Use system clipboard
+o.ignorecase = true -- Ignore case while searching
+o.smartcase = true -- Respect case if pattern has upper case characters
+o.splitright = true -- Put new window towards right on vertical split
+o.splitbelow = true -- Put new window towards bottom on horizontal split
+
 require("lazy").setup({
 	-- Keymaps
 	-- Pairs of bracket keymaps
@@ -29,5 +41,38 @@ require("lazy").setup({
 	{
 		"kylechui/nvim-surround",
 		opts = {},
+	},
+
+	-- UI/UX
+	-- Theme
+	{
+		"catppuccin/nvim",
+		config = function()
+			cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
+	-- UI for diagnostics, references, telescope results, quickfix list and location list
+	{
+		"folke/trouble.nvim",
+		lazy = false,
+		opts = {
+			focus = true, -- Move cursor to trouble window when open
+		},
+		keys = {
+			-- Goto previous trouble entry
+			{
+				"[q",
+				function()
+					require("trouble").prev({ jump = true })
+				end,
+			},
+			-- Goto next trouble entry
+			{
+				"]q",
+				function()
+					require("trouble").next({ jump = true })
+				end,
+			},
+		},
 	},
 })
