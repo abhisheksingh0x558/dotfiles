380bf32734715f9848e80f04ddd84cfe67d0ec6b
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-29 05:30:42 +0530

    nvim: configure keymaps

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,7 @@
 local fs = vim.fs
 local opt = vim.opt
 local g = vim.g
+local keymap = vim.keymap
 
 -- Plugin manager
 local lazypath = fs.normalize("~/.local/share/nvim/lazy/lazy.nvim")
@@ -8,3 +9,52 @@ opt.rtp:prepend(lazypath) -- Load plugin manager
 
 -- Code style
 g.editorconfig = true -- Editorconfig integration
+
+-- Keymaps
+g.mapleader = " " -- Leader key
+g.maplocalleader = "\\" -- Local leader key
+keymap.set("n", "<c-k>", "<up>") -- Command line prefix history search upward
+keymap.set("n", "<c-j>", "<down>") -- Command line prefix history search downward
+keymap.set("t", "<esc>", "<c-\\><c-n>") -- Switch from terminal mode to normal mode
+keymap.set("t", "<c-\\><c-n>", "<esc>") -- Send escape key to program running in terminal buffer
+
+require("lazy").setup({
+	-- Keymaps
+	-- Pairs of bracket keymaps
+	{
+		"tummetott/unimpaired.nvim",
+		opts = {},
+	},
+	-- Manipulate comments
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+	},
+	-- Manipulate surrounding pairs
+	{
+		"kylechui/nvim-surround",
+		opts = {},
+	},
+	-- Exchange text
+	{
+		"gbprod/substitute.nvim",
+		opts = {},
+	},
+	-- Manipulate text cases
+	{
+		"johmsalas/text-case.nvim",
+		opts = {},
+	},
+	-- Navigate with search labels
+	{
+		"folke/flash.nvim",
+		opts = {},
+	},
+	-- Manipulate alignments
+	"Vonr/align.nvim",
+	-- Find and replace in project
+	{
+		"MagicDuck/grug-far.nvim",
+		opts = {},
+	},
+})
