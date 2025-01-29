091bf7f7c26004e6821921ae80b3505276a44c0c
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-29 16:01:24 +0530

    nvim: improve ui and ux

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -8,6 +8,16 @@ vim.g.editorconfig = true -- EditorConfig integration
 vim.g.mapleader = " " -- Leader key
 vim.g.maplocalleader = "\\" -- Local leader key
 
+-- UI/UX
+vim.opt.shortmess:append("I") -- Disable intro message
+vim.o.number = true -- Enable line numbering
+vim.o.cursorline = true -- Enable line highlighting
+vim.o.clipboard = "unnamedplus" -- Use system clipboard
+vim.o.ignorecase = true -- Ignore case while searching
+vim.o.smartcase = true -- Respect case if pattern has upper case characters
+vim.o.splitright = true -- Put new window towards right on vertical split
+vim.o.splitbelow = true -- Put new window towards bottom on horizontal split
+
 require("lazy").setup({
 	-- Collection of plugins
 	{
@@ -42,4 +52,102 @@ require("lazy").setup({
 		},
 	},
 	{ "MagicDuck/grug-far.nvim", opts = {} }, -- Find and replace in project
+
+	-- UI/UX
+	-- Theme
+	{
+		"catppuccin/nvim",
+		config = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
+	{ "nvim-tree/nvim-web-devicons", opts = {} }, -- Icons
+	"MunifTanjim/nui.nvim", -- UI component library -- TODO: Use luarocks to manage this and remove from here
+	-- UI for messages, cmdline, and popupmenu
+	{
+		"folke/noice.nvim",
+		opts = {
+			cmdline = {
+				view = "cmdline", -- Show command line at the bottom
+			},
+		},
+	},
+	{ "Bekaboo/dropbar.nvim", opts = {} }, -- Breadcrumbs
+	{ "nvim-treesitter/nvim-treesitter-context", opts = {} }, -- Cursor context
+	-- UI for diagnostics, references, quickfix list and location list
+	{
+		"folke/trouble.nvim",
+		lazy = false,
+		opts = {
+			focus = true, -- Move cursor to trouble window when open
+		},
+		keys = {
+			-- Goto previous trouble entry
+			{
+				"[q",
+				function()
+					require("trouble").prev({ jump = true })
+				end,
+			},
+			-- Goto next trouble entry
+			{
+				"]q",
+				function()
+					require("trouble").next({ jump = true })
+				end,
+			},
+			{ "<Leader>od", "<Cmd>Trouble diagnostics toggle filter.buf=0<CR>" }, -- Toggle buffer diagnostics
+			{ "<Leader>oD", "<Cmd>Trouble diagnostics toggle<CR>" }, -- Toggle project diagnostics
+		},
+	},
+	-- Indentation guides
+	{
+		"folke/snacks.nvim",
+		opts = {
+			indent = { enabled = true },
+		},
+	},
+	-- Highlight todo comments
+	{
+		"folke/todo-comments.nvim",
+		lazy = false,
+		opts = {},
+		keys = {
+			-- Goto previous todo
+			{
+				"[t",
+				function()
+					require("todo-comments").jump_prev()
+				end,
+			},
+			-- Goto next todo
+			{
+				"]t",
+				function()
+					require("todo-comments").jump_next()
+				end,
+			},
+		},
+	},
+	-- Highlight lsp references
+	{
+		"RRethy/vim-illuminate",
+		keys = {
+			-- Goto previous reference
+			{
+				"[r",
+				function()
+					require("illuminate").goto_prev_reference(true)
+				end,
+			},
+			-- Goto next reference
+			{
+				"]r",
+				function()
+					require("illuminate").goto_next_reference(true)
+				end,
+			},
+		},
+	},
+	"HiPhish/rainbow-delimiters.nvim", -- Highlight brackets TODO: Remove this
 })
