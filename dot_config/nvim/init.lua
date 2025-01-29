547f1c8bdfd735987d8e93950b20815798f5e7ea
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-29 05:30:42 +0530

    nvim: configure keymaps

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,7 @@
 local fs = vim.fs
 local opt = vim.opt
 local g = vim.g
+local keymap = vim.keymap
 
 -- Plugin manager
 local lazypath = fs.normalize("~/.local/share/nvim/lazy/lazy.nvim")
@@ -9,6 +10,14 @@ opt.rtp:prepend(lazypath) -- Load plugin manager
 -- Code style
 g.editorconfig = true -- EditorConfig integration
 
+-- Keymaps
+g.mapleader = " " -- Leader key
+g.maplocalleader = "\\" -- Local leader key
+keymap.set("n", "<c-k>", "<up>") -- Command line prefix history search upward
+keymap.set("n", "<c-j>", "<down>") -- Command line prefix history search downward
+keymap.set("t", "<esc>", "<c-\\><c-n>") -- Switch from terminal mode to normal mode
+keymap.set("t", "<c-\\><c-n>", "<esc>") -- Send escape key to program running in terminal
+
 require("lazy").setup({
 	-- Collection of plugins
 	{
@@ -16,4 +25,22 @@ require("lazy").setup({
 		opts = {},
 	},
 	"echasnovski/mini.nvim",
+
+	-- Keymaps
+	{ "tummetott/unimpaired.nvim", opts = {} }, -- Pairs of bracket keymaps TODO: Remove this
+	{ "numToStr/Comment.nvim", opts = {} }, -- Manipulate comments TODO: Remove this
+	{ "kylechui/nvim-surround", opts = {} }, -- Manipulate surrounding pairs TODO: Remove this
+	{ "gbprod/substitute.nvim", opts = {} }, -- Exchange text TODO: Remove this
+	{ "johmsalas/text-case.nvim", opts = {} }, -- Manipulate text cases TODO: Remove this
+	{ "folke/flash.nvim", opts = {} }, -- Navigate with search labels
+	"Vonr/align.nvim", -- Manipulate alignments TODO: Remove this
+	-- Manipulate indentations
+	-- TODO: Do this with treesitter
+	{
+		"folke/snacks.nvim",
+		opts = {
+			scope = { enabled = true },
+		},
+	},
+	{ "MagicDuck/grug-far.nvim", opts = {} }, -- Find and replace in project
 })
