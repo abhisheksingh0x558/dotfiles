7e78357ec1adb6acc156f76ad8ac997ccd2cb43d
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-29 16:01:24 +0530

    nvim: improve ui and ux

--- chezmoi/dot_config/nvim/init.lua
+++ chezmoi/dot_config/nvim/init.lua
@@ -1,6 +1,8 @@
 local opt = vim.opt
 local fn = vim.fn
 local g = vim.g
+local o = vim.o
+local cmd = vim.cmd
 
 -- Plugin manager
 opt.rtp:prepend(fn.stdpath("data") .. "/lazy/lazy.nvim") -- Add plugin manager to runtime path
@@ -11,3 +13,24 @@ g.editorconfig = true -- Editorconfig integration
 -- Keymaps
 g.mapleader = " " -- Leader key
 g.maplocalleader = "\\" -- Local leader key
+
+-- UI/UX
+opt.shortmess:append("I") -- Disable intro message
+o.number = true -- Enable line numbering
+o.cursorline = true -- Enable line highlighting
+o.clipboard = "unnamedplus" -- Use system clipboard
+o.ignorecase = true -- Ignore case while searching
+o.smartcase = true -- Respect case if pattern has upper case characters
+o.splitright = true -- Put new window towards right on vertical split
+o.splitbelow = true -- Put new window towards bottom on horizontal split
+
+require("lazy").setup({
+	-- UI/UX
+	{
+		-- Theme
+		"catppuccin/nvim",
+		config = function()
+			cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
+})
