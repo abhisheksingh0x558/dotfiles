c9d841031aa009d8443dc79efe1b414b8efa723d
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-29 15:22:04 +0530

    emacs: setup ui and ux packages

--- private_dot_config/emacs/early-init.el
+++ private_dot_config/emacs/early-init.el
@@ -2,3 +2,8 @@
 
 ;; Package manager
 (setq package-enable-at-startup nil) ; Disable package loading at startup
+
+;; UI/UX
+(menu-bar-mode 0) ; Disable menu bar
+(tool-bar-mode 0) ; Disable tool bar
+(scroll-bar-mode 0) ; Disable scroll bar
--- private_dot_config/emacs/init.el
+++ private_dot_config/emacs/init.el
@@ -51,3 +51,26 @@
 
 ;;; Code style
 (editorconfig-mode) ; EditorConfig integration
+
+;;; UI/UX
+(setq inhibit-startup-screen t) ; Disable startup screen
+(setq initial-scratch-message nil) ; Disable scratch buffer message
+(global-display-line-numbers-mode) ; Enable line numbering
+(global-hl-line-mode) ; Enable line highlighting
+(setq evil-vsplit-window-right t) ; Put new window on right on vertical split
+(setq evil-split-window-below t) ; Put new window on bottom on horizontal split
+(setq create-lockfiles nil) ; Do not create lockfiles ; TODO: Make this consistent with Neovim
+(setq backup-directory-alist `(("." . ,(expand-file-name "backups" user-emacs-directory)))) ; Backup directory ; TODO: Make this consistent with Neovim
+(setq auto-save-file-name-transforms `((".*" ,(expand-file-name "autosaves" user-emacs-directory) t))) ; Autosave directory ; TODO: Make this consistent with Neovim
+(recentf-mode) ; Track recent files
+;; Theme
+(leaf doom-themes
+  :config (load-theme 'doom-tokyo-night t))
+;; Modeline
+(leaf doom-modeline
+  :config (doom-modeline-mode))
+(leaf breadcrumb :config (breadcrumb-mode)) ; Breadcrumbs ; TODO: Replace with lsp alternative
+(leaf highlight-indent-guides :config (highlight-indent-guides-mode)) ; Indentation guides
+(leaf hl-todo :config (global-hl-todo-mode)) ; Highlight to do comments
+(leaf symbol-overlay :config (symbol-overlay-mode)) ; Highlight lsp references ; TODO: Replace with lsp alternative
+(leaf rainbow-delimiters :config (rainbow-delimiters-mode)) ; Highlight brackets
