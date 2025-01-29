5b31e8cb77cd0a35c7b1b2c8b58854c607e24823
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-29 16:01:24 +0530

    nvim: improve ui and ux

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,6 +2,8 @@ local fs = vim.fs
 local opt = vim.opt
 local g = vim.g
 local keymap = vim.keymap
+local o = vim.o
+local cmd = vim.cmd
 
 -- Plugin manager
 local lazypath = fs.normalize("~/.local/share/nvim/lazy/lazy.nvim")
@@ -17,6 +19,16 @@ keymap.set("n", "<c-k>", "<up>") -- Command line prefix history search upward
 keymap.set("n", "<c-j>", "<down>") -- Command line prefix history search downward
 keymap.set("t", "<esc>", "<c-\\><c-n>") -- Switch from terminal mode to normal mode
 keymap.set("t", "<c-\\><c-n>", "<esc>") -- Send escape key to program running in terminal
+
+-- UI/UX
+opt.shortmess:append("I") -- Disable intro message
+o.number = true -- Enable line numbering
+o.cursorline = true -- Enable line highlighting
+o.clipboard = "unnamedplus" -- Use system clipboard
+o.ignorecase = true -- Ignore case while searching
+o.smartcase = true -- Respect case if pattern has upper case characters
+o.splitright = true -- Put new window towards right on vertical split
+o.splitbelow = true -- Put new window towards bottom on horizontal split
 
 require("lazy").setup({
 	-- Collection of plugins
@@ -43,4 +55,71 @@ require("lazy").setup({
 		},
 	},
 	{ "MagicDuck/grug-far.nvim", opts = {} }, -- Find and replace in project
+
+	-- UI/UX
+	-- Theme
+	{
+		"catppuccin/nvim",
+		config = function()
+			cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
+	{ "nvim-tree/nvim-web-devicons", opts = {} }, -- Icons
+	"MunifTanjim/nui.nvim", -- UI component library -- TODO: Use luarocks to manage this and remove from here
+	-- UI for messages, cmdline, and popupmenu
+	{
+		"folke/noice.nvim",
+		opts = {
+			cmdline = {
+				view = "cmdline", -- Show command line at the bottom
+			},
+		},
+	},
+	{ "Bekaboo/dropbar.nvim", opts = {} }, -- Breadcrumbs
+	{ "nvim-treesitter/nvim-treesitter-context", opts = {} }, -- Cursor context
+	-- UI for diagnostics, references, quickfix list and location list
+	{
+		"folke/trouble.nvim",
+		lazy = false,
+		opts = {
+			focus = true, -- Move cursor to trouble window when open
+		},
+		keys = {
+			-- Goto previous trouble entry
+			{
+				"[q",
+				function()
+					require("trouble").prev({ jump = true })
+				end,
+			},
+			-- Goto next trouble entry
+			{
+				"]q",
+				function()
+					require("trouble").next({ jump = true })
+				end,
+			},
+			{ "<leader>od", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>" }, -- Toggle buffer diagnostics
+			{ "<leader>oD", "<cmd>Trouble diagnostics toggle<cr>" }, -- Toggle project diagnostics
+		},
+	},
+	-- Indentation guides
+	{
+		"folke/snacks.nvim",
+		opts = {
+			indent = { enabled = true },
+		},
+	},
+	-- Highlight todo comments TODO: Remove this
+	{
+		"folke/todo-comments.nvim",
+		lazy = false,
+		opts = {},
+		keys = {
+			{ "[t", '<cmd>lua require("todo-comments").jump_prev()<cr>' }, -- Goto previous todo
+			{ "]t", '<cmd>lua require("todo-comments").jump_next()<cr>' }, -- Goto next todo
+		},
+	},
+	"RRethy/vim-illuminate", --  Highlight word under cursor TODO: Remove this
+	"HiPhish/rainbow-delimiters.nvim", -- Highlight brackets TODO: Remove this
 })
