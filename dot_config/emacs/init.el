b4de90f52b95696c5cdc1691800e8870f1cb2d00
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-30 22:15:06 +0530

    emacs: setup package manager

---
+++ dot_config/emacs/init.el
@@ -0,0 +1,20 @@
+;; -*- lexical-binding: t -*-
+
+;;; Package manager
+(setq elpaca-installer-version 0.11)
+;; Load package manager
+(let ((elpaca-path (expand-file-name "elpaca/repos/elpaca" user-emacs-directory)))
+  (add-to-list 'load-path elpaca-path))
+(unless (require 'elpaca-autoloads nil t)
+  (require 'elpaca)
+  (elpaca-generate-autoloads "elpaca" elpaca-path))
+(require 'elpaca-autoloads)
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca (elpaca :repo "https://github.com/progfolio/elpaca.git" :build (:not elpaca--activate-package)))
+(elpaca leaf
+  (require 'leaf)
+  (setq leaf-defaults '(:elpaca t)))
+(elpaca leaf-keywords
+  (require 'leaf-keywords)
+  (leaf-keywords-init))
+(elpaca-wait)
