94b65bcdf56fbcde755a2b2581b8f6d8b16ae31d
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-30 06:46:11 +0530

    macos: install shells

--- macos.nix
+++ macos.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   # Nix Darwin version
   # WARNING: Do not change this without referring to release notes for Nix Darwin
   system.stateVersion = 5;
@@ -9,4 +9,12 @@
   # Homebrew
   homebrew.enable = true;
   homebrew.onActivation.cleanup = "zap"; # Uninstall stale applications
+
+  environment.systemPackages = with pkgs; [
+    nushell # Shell
+    zsh # Shell
+  ];
+
+  # Permissible shells
+  environment.shells = with pkgs; [ nushell zsh ];
 }
