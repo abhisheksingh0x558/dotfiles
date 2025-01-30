7e30adce5c5cc43f75ebf21bd1df8c9c6a32660e
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-30 22:12:49 +0530

    nvim: setup plugin manager

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,11 @@
+-- Plugin manager
+vim.opt.runtimepath:prepend(vim.fs.normalize("~/.local/share/nvim/lazy/lazy.nvim")) -- Load plugin manager
+
+require("lazy").setup({
+	-- Collection of plugins
+	{
+		"folke/snacks.nvim",
+		opts = {},
+	},
+	"echasnovski/mini.nvim",
+})
