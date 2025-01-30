4f64a09fcf0aad148659028ff796f5a9b02ab7c9
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-01-30 22:12:49 +0530

    nvim: setup plugin manager

---
+++ chezmoi/dot_config/nvim/init.lua
@@ -0,0 +1,17 @@
+local fs = vim.fs
+local uv = vim.uv
+local system = vim.system
+local opt = vim.opt
+
+-- Plugin manager
+local lazypath = fs.normalize("~/.local/share/nvim/lazy/lazy.nvim")
+local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+-- Install plugin manager
+if not uv.fs_stat(lazypath) then
+	local result = system({ "git", "clone", lazyrepo, lazypath }):wait()
+	if result.code ~= 0 then
+		os.exit(result.code)
+	end
+end
+-- Load plugin manager
+opt.rtp:prepend(lazypath)
