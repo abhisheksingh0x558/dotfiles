147f6bd2c8ec989a9e57b3c5dcaef608cc9e3e36
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-02-02 21:58:21 +0530

    nvim: setup go support

--- chezmoi/dot_config/nvim/init.lua
+++ chezmoi/dot_config/nvim/init.lua
@@ -187,6 +187,7 @@ require("lazy").setup({
 					"lua",
 					"haskell",
 					"rust",
+					"go",
 				},
 			})
 		end,
@@ -202,6 +203,7 @@ require("lazy").setup({
 			lspconfig.lua_ls.setup({})
 			lspconfig.hls.setup({})
 			lspconfig.rust_analyzer.setup({})
+			lspconfig.gopls.setup({})
 		end,
 	},
 
@@ -223,6 +225,7 @@ require("lazy").setup({
 				lua = { "luacheck" },
 				haskell = { "hlint" },
 				rust = { "clippy" },
+				go = { "staticcheck" },
 			}
 		end,
 	},
@@ -238,6 +241,7 @@ require("lazy").setup({
 				lua = { "stylua" },
 				haskell = { "fourmolu" },
 				rust = { "rustfmt" },
+				go = { "gofumpt" },
 			},
 		},
 	},
