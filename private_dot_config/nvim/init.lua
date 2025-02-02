766dd51b4531c5cc194e456aa3f29f32144d8f68
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-02-02 22:07:09 +0530

    nvim: setup lsp integration

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -595,4 +595,19 @@ require("lazy").setup({
 			},
 		},
 	},
+
+	-- LSP integration
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- Triggers for code lenses
+			vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+				group = vim.api.nvim_create_augroup("lsp-code-lens", {}),
+				callback = function()
+					vim.lsp.codelens.refresh()
+				end,
+			})
+			vim.lsp.inlay_hint.enable() -- Inlay hints
+		end,
+	},
 })
