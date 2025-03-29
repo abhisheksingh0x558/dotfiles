b01e8f30cc7a72338290af5bfd56d671b3304016
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-03-30 01:14:17 +0530

    nvim: setup debugger

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -322,4 +322,9 @@ require("lazy").setup({
 
 	-- Test runner
 	{ "nvim-neotest/neotest", opts = {} },
+
+	-- Debugger
+	"mfussenegger/nvim-dap",
+	{ "rcarriga/nvim-dap-ui", opts = {} },
+	{ "theHamsta/nvim-dap-virtual-text", opts = {} },
 })
