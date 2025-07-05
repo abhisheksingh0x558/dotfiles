10166dc5c438ace98ff01c12f23096a9d35a1204
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-07-06 00:18:34 +0530

    nvim: setup rust support

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -667,6 +667,10 @@ require("lazy").setup({
 
 	-- Haskell support
 	"mrcjkb/haskell-tools.nvim", -- TODO: Disable server triggered
+
+	-- Rust support
+	"mrcjkb/rustaceanvim", -- TODO: Disable server triggered
+	{ "Saecki/crates.nvim", opts = {} },
 })
 
 -- Language configurations
@@ -732,6 +736,13 @@ local languages = {
 	-- TODO: Setup Common Lisp
 
 	-- TODO: Setup Zig
+
+	-- Rust
+	rust = {
+		language_server = "rust_analyzer",
+		linters = { "clippy" },
+		formatters = { "rustfmt" },
+	},
 }
 
 -- Setup language tools
