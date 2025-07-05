29db34cc26c68fd2c2972594bb53ecf1174a8f1e
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-07-06 00:15:51 +0530

    nvim: setup zsh support

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -438,6 +438,7 @@ require("lazy").setup({
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter").install("all") -- Install all parsers asynchronously
+			vim.treesitter.language.register("bash", "zsh") -- TODO: Remove this when zsh parser is supported officially
 		end,
 	},
 	-- Text objects
@@ -564,6 +565,13 @@ local languages = {
 		language_server = "nushell",
 		-- TODO: Add linters
 		-- TODO: Add formatters
+	},
+
+	-- Zsh
+	zsh = {
+		language_server = "bashls",
+		linters = { "shellcheck" },
+		formatters = { "shfmt" },
 	},
 }
 
