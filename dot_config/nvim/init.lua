fa211ae6fbdf53a175049045ae5e660e65230eee
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-07-06 00:22:58 +0530

    nvim: setup purescript support

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -546,6 +546,13 @@ local languages = {
 	-- TODO: Setup Clojure
 
 	-- TODO: Setup Kotlin
+
+	-- PureScript
+	purescript = {
+		language_server = "purescriptls",
+		-- TODO: Add linters
+		formatters = { "purs-tidy" },
+	},
 }
 
 -- Setup language tools
