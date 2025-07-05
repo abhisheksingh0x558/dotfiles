eea7ffe81c1dc5ae858bffb65e9fe34e330046aa
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-07-06 00:17:29 +0530

    nvim: setup lua support

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -556,6 +556,9 @@ require("lazy").setup({
 
 	-- GitHub client
 	{ "pwntester/octo.nvim", opts = {} },
+
+	-- Lua support
+	"folke/lazydev.nvim", -- TODO: Disable server triggered
 })
 
 -- Language configurations
@@ -595,6 +598,13 @@ local languages = {
 		language_server = "nil_ls",
 		linters = { "statix" },
 		formatters = { "nixfmt" },
+	},
+
+	-- Lua
+	lua = {
+		language_server = "lua_ls",
+		linters = { "luacheck" },
+		formatters = { "stylua" },
 	},
 }
 
