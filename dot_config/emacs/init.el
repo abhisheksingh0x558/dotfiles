45b5cef863bae0276f4a598b338c4d92b377dd26
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-07-06 03:50:55 +0530

    emacs: setup nix support

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -201,7 +201,12 @@
 (leaf forge)
 
 ;;; Language configurations
-(defvar languages '())
+(defvar languages
+  '(;; Nix
+    (nix-ts-mode
+     :language-server nix-nil
+     :linters (statix)
+     :formatters (nixfmt)))
 
 ;;; Setup language tools
 (setq apheleia-mode-alist '())
@@ -235,3 +240,7 @@
 ;;; Setup tools for all configured languages
 (dolist (language languages)
   (setup-language (car language) (cdr language)))
+
+;;; Nix support
+(leaf nix-mode)
+(leaf nix-ts-mode)
