805f9c6d7e30e0b8828debd85c4b5f96602036da
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-07-06 03:51:42 +0530

    emacs: setup rust support

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -213,7 +213,13 @@
     (haskell-ts-mode
      :language-server lsp-haskell
      :linters (haskell-hlint)
-     :formatters (fourmolu)))
+     :formatters (fourmolu))
+
+    ;; Rust
+    (rust-ts-mode
+     :language-server rust-analyzer
+     :linters (rust-clippy)
+     :formatters (rustfmt)))
 
 ;;; Setup language tools
 (setq apheleia-mode-alist '())
@@ -255,3 +261,6 @@
 ;;; Haskell support
 (leaf haskell-mode)
 (leaf haskell-ts-mode)
+
+;;; Rust support
+(leaf rust-mode)
