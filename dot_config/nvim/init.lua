ca7bd4a2d4c8a1778d6f94a9931223ffd139b205
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-07-25 07:13:33 +0530

    nvim: setup fennel support

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -606,6 +606,13 @@ local languages = {
 		linters = { "luacheck" },
 		formatters = { "stylua" },
 	},
+
+	-- Fennel
+	fennel = {
+		language_server = "fennel_ls",
+		linters = { "fennel" },
+		formatters = { "fnlfmt" },
+	},
 }
 
 -- Setup language tools
