88018855b16866f2c976e6acfcb3b191680e7acd
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-11-27 13:29:31 +0530

    nvim: setup treesitter plugins

--- private_dot_config/nvim/lua/my/spec/spec.lua
+++ private_dot_config/nvim/lua/my/spec/spec.lua
@@ -165,4 +165,22 @@ return {
 
 	-- Outliner
 	{ "stevearc/aerial.nvim", opts = {} },
+
+	-- Treesitter integration
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		branch = "main", -- TODO: Remove this when main is set as default branch
+		build = ":TSUpdate",
+	},
+	-- Text objects
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main", -- TODO: Remove this when main is set as default branch
+		opts = {
+			select = {
+				lookahead = true, -- Automatically jump forward to text object
+			},
+		},
+	},
 }
