2fcd98e8e5601dcfb032a918b8ffd037ee4653e5
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-11-27 13:13:32 +0530

    nvim: setup key mapping plugins

--- private_dot_config/nvim/lua/my/spec/spec.lua
+++ private_dot_config/nvim/lua/my/spec/spec.lua
@@ -1,7 +1,19 @@
 -- Plugin specifications
 return {
 	-- Collection of plugins
-	{ "folke/snacks.nvim", opts = {} },
+	{
+		"folke/snacks.nvim",
+		opts = {
+			-- Key mappings
+			-- Manipulate indentations
+			scope = {
+				enabled = true,
+				keys = {
+					jump = {},
+				},
+			},
+		},
+	},
 	"echasnovski/mini.nvim",
 
 	-- Lua libraries
@@ -10,4 +22,13 @@ return {
 
 	-- Lua transpiler
 	{ "rktjmp/hotpot.nvim", opts = {} },
+
+	-- Key mappings
+	{ "numToStr/Comment.nvim", opts = {} }, -- Manipulate comments
+	{ "kylechui/nvim-surround", opts = {} }, -- Manipulate surrounding pairs
+	{ "gbprod/substitute.nvim", opts = {} }, -- Exchange text
+	{ "johmsalas/text-case.nvim", opts = {} }, -- Manipulate text cases
+	{ "folke/flash.nvim", opts = {} }, -- Navigate with search labels
+	"Vonr/align.nvim", -- Manipulate alignments
+	{ "MagicDuck/grug-far.nvim", opts = {} }, -- Find and replace in project
 }
