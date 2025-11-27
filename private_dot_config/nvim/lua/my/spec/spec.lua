c78ee14d006e4c14dc47bdef934a4c5627627dce
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-11-27 13:12:19 +0530

    nvim: setup lua libraries

--- private_dot_config/nvim/lua/my/spec/spec.lua
+++ private_dot_config/nvim/lua/my/spec/spec.lua
@@ -3,4 +3,8 @@ return {
 	-- Collection of plugins
 	{ "folke/snacks.nvim", opts = {} },
 	"echasnovski/mini.nvim",
+
+	-- Lua libraries
+	"kevinhwang91/promise-async", -- TODO: Use luarocks to manage this and remove from here
+	"nvim-neotest/nvim-nio", -- TODO: Use luarocks to manage this and remove from here
 }
