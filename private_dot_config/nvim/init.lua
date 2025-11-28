cfe0679f4742de772b1a4b4cc4f7b4269d758f19
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-11-28 13:25:42 +0530

    nvim: load core modules

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,13 @@
+-- Load modules
+local function load(directory)
+	local namespace = directory:match(".*/lua/(.*)"):gsub("/", ".")
+	local modules = vim.fs.find(function(name)
+		return name:match("%.lua$")
+	end, { type = "file", path = directory, limit = math.huge })
+	for _, module in ipairs(modules) do
+		require(namespace .. "." .. module:match("([^/]+)%.lua$"))
+	end
+end
+
+-- Load core modules
+load(vim.fn.stdpath("config") .. "/lua/my/core")
