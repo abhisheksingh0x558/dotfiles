9586f7a46c636407011ea5f99744fabd934993eb
Abhishek Singh <abhisheksingh0x558@proton.me>
2025-11-28 13:40:04 +0530

    emacs: load core modules

--- private_dot_config/emacs/init.el
+++ private_dot_config/emacs/init.el
@@ -3,3 +3,11 @@
 ;; Elisp path
 (defvar my-elisp-path (expand-file-name "elisp" user-emacs-directory))
 (add-to-list 'load-path my-elisp-path) ; Custom modules
+
+;; Load modules
+(defun my-load(prefix)
+  (dolist (module (directory-files my-elisp-path nil (format "%s-.+\\.el$" (regexp-quote prefix))))
+    (require (intern (file-name-base module)))))
+
+;; Load core modules
+(my-load "my-core")
